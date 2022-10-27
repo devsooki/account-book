0121ab12ef60bcea3dc378c074a58e8071a7260b
devsooki <devsooki@gmail.com>
2022-10-27 16:14:43 +0900

    feat: init setting

---
+++ src/style/GlobalStyle.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    font-family: 'Nanum Gothic', sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html, #root {
+    height: 100%;
+  }
+  body {
+    height: 100%;
+    background-color: #fce4ea;
+  }
+  ul,li {
+    list-style: none;
+  }
+  input, textarea {
+    outline: none;
+  }
+  button {
+    border: 0;
+    cursor: pointer;
+  }
+`;
