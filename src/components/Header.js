6ec3c65a684407e39696b953014c527c4ab0828f
devsooki <devsooki@gmail.com>
2022-10-28 23:32:25 +0900

    feat: header

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+// style
+import styled from 'styled-components';
+
+const Header = () => {
+  return (
+    <Container>
+      <h1>가계부</h1>
+      <span>수입부터 소득, 저축까지!<br />손 쉽게 관리하세요!</span>
+    </Container>
+  );
+};
+
+export default Header;
+
+const Container = styled.header`
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  width: 800px;
+  height: 80px;
+  color: #333;
+
+  h1 {
+    margin-right: 10px;
+    font-size: 30px;
+    font-family: 'Fira Sans', sans-serif;
+  }
+  span {
+    font-size: 12px;
+  }
+`
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import Header from 'components/Header';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import GlobalStyle from './style/GlobalStyle';
@@ -6,5 +7,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
+    <Header />
   </React.StrictMode>
 );
