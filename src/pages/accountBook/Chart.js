08e1e7a75f882b2bc6ceeff308335e87fe9c31a7
devsooki <devsooki@gmail.com>
2022-11-09 23:13:40 +0900

    feat: chart library

--- src/pages/accountBook/Chart.js
+++ src/pages/accountBook/Chart.js
@@ -1,51 +1,18 @@
 import React from 'react'
+import { PieChart } from 'react-minimal-pie-chart'
 import styled from 'styled-components'
 
 const Chart = () => {
   return (
     <Container>
-      <ChartContent>
-        <svg viewBox="0 0 200 200">
-          <circle cx="100" cy="100" r="90" fill="none" stroke="beige" strokeWidth="20" />
-          <circle
-            cx="100"
-            cy="100"
-            r="90"
-            fill="none"
-            stroke="green"
-            strokeWidth="20"
-            strokeDasharray={2 * Math.PI * 90 * 0.75}
-          />
-        </svg>
-      </ChartContent>
-      <ChartContent>
-        <svg viewBox="0 0 200 200">
-          <circle cx="100" cy="100" r="90" fill="none" stroke="beige" strokeWidth="20" />
-          <circle
-            cx="100"
-            cy="100"
-            r="90"
-            fill="none"
-            stroke="green"
-            strokeWidth="20"
-            strokeDasharray={2 * Math.PI * 90 * 0.75}
-          />
-        </svg>
-      </ChartContent>
-      <ChartContent>
-        <svg viewBox="0 0 200 200">
-          <circle cx="100" cy="100" r="90" fill="none" stroke="beige" strokeWidth="20" />
-          <circle
-            cx="100"
-            cy="100"
-            r="90"
-            fill="none"
-            stroke="green"
-            strokeWidth="20"
-            strokeDasharray={2 * Math.PI * 90 * 0.75}
-          />
-        </svg>
-      </ChartContent>
+      <PieChart
+        data={[
+          { title: 'One', value: 10, color: '#FF8DC7' },
+          { title: 'Two', value: 15, color: '#FFACC7' },
+          { title: 'Three', value: 20, color: '#FFDDD2' },
+        ]}
+        animate
+      />
     </Container>
   )
 }
@@ -57,12 +24,10 @@ const Container = styled.div`
   align-items: center;
   justify-content: space-around;
   margin: 0 auto;
+  padding: 30px 0;
   min-width: 375px;
   max-width: 800px;
   width: 100%;
   height: 400px;
   background-color: #fff;
-`
-const ChartContent = styled.div`
-  width: 200px;
 `
